1b6789f1776d29a8f5228208da789f2eb8365f35
Hamza Mehboob <hamzamehboob493@gmail.com>
2025-07-12 01:45:18 +0500

    completed analytics for organization

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "chartkick" # @5.0.1
 pin "chart.js" # @4.5.0
 pin "date-fns" # @4.1.0
-pin "chartjs-adapter-date-fns" # @3.0.0
+pin "chartjs-adapter-date-fns", to: "chartjs-adapter-date-fns.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.4
